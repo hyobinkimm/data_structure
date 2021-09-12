cd6c13c488c7a8131a6abae210bf9aa53ba0850f
hyobinkimm <sowhat0516@gmail.com>
2021-09-12 15:33:12 +0900

    stackArray

---
+++ stack.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+inline void error(const char* message) {
+	printf("%d\n", message);
+}
+
+const int MAX_STACK_SIZE = 20;
+
+class ArrayStack {
+	int top;
+	int data[MAX_STACK_SIZE];
+
+public:
+	ArrayStack() { top = -1; }
+	~ArrayStack() {}
+	bool isEmpty() { return top == -1; }
+	bool isFull() { return top == MAX_STACK_SIZE - 1; }
+
+	void push(int e) {
+		if (isFull())error("스택 포화 에러");
+		data[++top] = e;
+	}
+
+	int pop() {
+		if (isEmpty())error("스택 공백 에러");
+		return data[top--];
+	}
+
+	int peek() {
+		if (isEmpty()) error("스택 공백 에러");
+		return data[top];
+	}
+
+	void display() {
+		printf("[스택 항목의 수 = %2d] ==> ", top + 1);
+		for (int i = 0; i <= top; i++)
+		{
+			printf("<%2d>", data[i]);
+		}
+		printf("\n");
+	}
+};
+
+int main() {
+	ArrayStack stack;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		stack.push(i);
+	}
+	stack.display();
+	printf("%2d를 꺼냈습니다.\n", stack.pop());
+	printf("%2d를 꺼냈습니다.\n", stack.pop());
+	printf("%2d를 꺼냈습니다.\n", stack.pop());
+
+	stack.display();
+
+}
